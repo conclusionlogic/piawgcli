c375e7eed896974a237187b5e9b3350506ef3bf5
Derek Battams <derek@battams.ca>
2021-05-15 17:04:10 -0400

    build fixes

--- build-all.sh
+++ build-all.sh
@@ -7,14 +7,13 @@ windows/amd64"
 for PLATFORM in $PLATFORMS; do
         GOOS=${PLATFORM%/*}
         GOARCH=${PLATFORM#*/}
-        FILEPATH="$PROJECT_DIR/artifacts/${GOOS}-${GOARCH}"
-        #echo $FILEPATH
+        FILEPATH="$PROJECT_DIR/artifacts/"
         mkdir -p $FILEPATH
         BIN_FILE_NAME="$FILEPATH/${BIN_FILE_NAME_PREFIX}"
-        #echo $BIN_FILE_NAME
         if [[ "${GOOS}" == "windows" ]]; then BIN_FILE_NAME="${BIN_FILE_NAME}.exe"; fi
         CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${BIN_FILE_NAME}"
-        #echo $CMD
         echo "${CMD}"
         eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
+        gzip -c $BIN_FILE_NAME > ${GOOS}-${GOARCH}-${BIN_FILE_NAME}.gz
+        rm -f $BIN_FILE_NAME
 done
